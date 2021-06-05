dc4ba233e36f89b48ce7340e10ae5ac3277b0989
Joepolymath <joshuaajagbe96@gmail.com>
2021-06-05 01:41:44 +0100

    added database connection for the appointment page

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
-from flask import Flask, render_template
+from enum import unique
+from flask import Flask, render_template, redirect
+from flask.helpers import url_for
 from flask_sqlalchemy import SQLAlchemy
+from forms import AppointmentForm, RegistrationForm, LoginForm
+from flask import request
+import os
 
 
 app = Flask(__name__)
+
+# file location or directory
+project_dir = os.path.dirname(os.path.abspath(__file__))
+
+# database file directory
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "appointments.db"))
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config["SECRET_KEY"] = 'bd72b0f78069cb14a807137debda0239'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# connecting the database file
+db = SQLAlchemy(app)
+
+class AppointmentBook(db.Model):
+    # id = db.Column(db.Integer(), primary_key=True, nullable=False, unique=True, autoincrement=True)
+    first_name = db.Column(db.String(30), unique=False, nullable=False, primary_key=True)
+    last_name = db.Column(db.String(30), unique=False, nullable=False)
+    email = db.Column(db.String(30), unique=True, nullable=False)
+
+    def __repr__(self):
+        return "<First name: {}>".format(self.first_name)
+
 
 @app.route('/')
 @app.route('/home')
@@ -21,9 +47,37 @@ def about():
 def barbers():
     return render_template('barbers.html')
 
-@app.route('/appointment')
+@app.route('/appointment', methods=['GET', 'POST'])
 def appointment():
+    # form = AppointmentForm()
     return render_template('appointment.html')
+
+# @app.route('/appointmentlist')
+# def appointmentList():
+    
+    
+
+@app.route('/sub', methods=['GET', 'POST'])
+def submitapp():
+    if request.form:
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        email = request.form.get('email')
+        appointment = AppointmentBook(first_name=first_name, last_name=last_name, email=email)
+        db.session.add(appointment)
+        db.session.commit()
+        appoint = AppointmentBook.query.all()
+        return redirect(url_for('home'))
+    
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
 
 @app.route('/contact')
 def contact():
--- forms.py
+++ forms.py
@@ -1,4 +1,40 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
+from wtforms.fields.simple import TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+import phonenumbers
+
+class RegistrationForm(FlaskForm):
+    admin_username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    admin_email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class AppointmentForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired(), Length(min=2, max=15)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=15)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    phone = StringField('Phone')
+    details = TextAreaField('Give us more details')
+    submit = SubmitField('Get thy Appointment')
+
+    def validate_phone(form, field):
+        if len(field.data) > 16:
+            raise ValidationError('Invalid phone number.')
+        try:
+            input_number = phonenumbers.parse(field.data)
+            if not (phonenumbers.is_valid_number(input_number)):
+                raise ValidationError('Invalid phone number.')
+        except:
+            input_number = phonenumbers.parse("+1"+field.data)
+            if not (phonenumbers.is_valid_number(input_number)):
+                raise ValidationError('Invalid phone number.')
+
 
